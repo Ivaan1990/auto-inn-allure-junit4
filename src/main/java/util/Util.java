0ab2f95b8198b5e09dc25ecfadd0ed61dffb3212
Ivaan1990 <ivaan7845@gmail.com>
2019-04-24 00:13:23 +0300

    Refactoring

---
+++ src/main/java/util/Util.java
@@ -0,0 +1,44 @@
+package util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class Util {
+
+    private static WebDriver driver = null;
+    private static Properties properties = TestProperties.getInstance().getProperties();
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static WebDriver createDriver(){
+        switch (properties.getProperty("browser")){
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", properties.getProperty("webdriver.gecko.driver"));
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.chrome.driver"));
+                driver = new ChromeDriver();
+                break;
+        }
+
+        driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(properties.getProperty("app.url"));
+        return driver;
+    }
+
+    public static void quitDriver(){
+        if (driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+}
